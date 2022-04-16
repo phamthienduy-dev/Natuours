8ac83577b38f7162fd2c4d840d81d42fb7170065
phamthienduy-dev <duyphamthien@outlook.com>
2022-04-16 14:26:07 +0700

    Code Update

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,12 +6,18 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  aliasTopTours
+  aliasTopTours,
+  getTourStats,
+  getMonthlyPlan
 } = require("../controllers/tourController");
 
 const router = express.Router();
 
 router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
+
+router.route("/tour-stats").get(getTourStats);
+
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 
 router
   .route("/")
